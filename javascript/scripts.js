fa944855a297e2ce06ab8c624ada87c733faf5ed
Jonathan Lam <jdlam420@gmail.com>
2015-10-26 13:20:01 -0400

    adds content to page

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,1 +1,20 @@
 console.log('loaded');
+
+$(document).ready(function(){
+
+  bindNavButtons('#ed-btn', '#education');
+  bindNavButtons('#exp-btn', '#experience');
+  bindNavButtons('#proj-btn', '#projects');
+  bindNavButtons('#skill-btn', '#skills');
+  bindNavButtons('#int-btn', '#interests');
+
+})
+
+
+function bindNavButtons(button, div) {
+  $(button).click(function(){
+    $('html, body').animate({
+      scrollTop: $(div).offset().top
+    }, 1000, 'easeOutCubic');
+  });
+}
