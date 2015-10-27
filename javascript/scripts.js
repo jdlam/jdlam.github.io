641de0166d25747dedaf4a0f9d31d607e71c5305
Jonathan Lam <jdlam420@gmail.com>
2015-10-27 01:45:38 -0400

    fixes scrolling for larger screen sizes

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   bindNavButtons('#intro-btn', '#intro', 50);
   bindNavButtons('#ed-btn', '#education', 0);
   bindNavButtons('#exp-btn', '#experience', 50);
-  bindNavButtons('#proj-btn', '#projects', 125);
+  bindNavButtons('#proj-btn', '#projects', 100);
   bindNavButtons('#skill-btn', '#skill-top', 75);
 
 })
@@ -14,6 +14,8 @@ $(document).ready(function(){
 function bindNavButtons(button, div, val) {
   if($(window).width() < 790 && div === '#projects') {
     val -= 150;
+  } else if($(window).width() > 1000 && div === '#projects') {
+    val = 25;
   }
   $(button).click(function(){
     $('html, body').animate({
